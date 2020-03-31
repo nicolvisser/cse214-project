439d591e32b0ca479941da02d570cbc2206d85ae
Nicol Visser <nicolvisser@yahoo.com>
2020-03-31 23:20:47 +0200

    Minor changes to defaultCritter

--- src/DefaultCritter.java
+++ src/DefaultCritter.java
@@ -5,44 +5,39 @@ public class DefaultCritter extends Object2D implements Critter {
 
     private static final long serialVersionUID = 1L;
     private static final int DEFAULT_HEALTH_POINTS = 100;
-    private static final int DEFAULT_COLLISSION_RADIUS = 5;
+    private static final int DEFAULT_COLLISION_RADIUS = 5;
 
     public int healthPoints;
     public double collisionRadius;
 
     public DefaultCritter() {
         super();
-        healthPoints = DEFAULT_HEALTH_POINTS; // Default health points
-        collisionRadius = DEFAULT_COLLISSION_RADIUS; // Default collision radius
+        healthPoints = DEFAULT_HEALTH_POINTS;
+        collisionRadius = DEFAULT_COLLISION_RADIUS;
     }
 
     public DefaultCritter(Vector2D position, double orientation) {
         super(position, orientation);
-
-        healthPoints = DEFAULT_HEALTH_POINTS; // Default health points
-        collisionRadius = DEFAULT_COLLISSION_RADIUS; // Default collision radius
+        healthPoints = DEFAULT_HEALTH_POINTS;
+        collisionRadius = DEFAULT_COLLISION_RADIUS;
     }
 
     public void takeDamage(int damagePoints) {
         healthPoints -= damagePoints;
     }
 
+    // draw simple default representation of object for debugging
     public void draw() {
         StdDraw.setPenColor(StdDraw.RED);
-
         // draw line for line of sight
         Vector2D aimTarget = position.add(lookVector().scale(100));
         StdDraw.line(position.x, position.y, aimTarget.x, aimTarget.y);
-
         // draw circle for body
         StdDraw.filledCircle(position.x, position.y, collisionRadius);
     }
 
     public boolean isCollidingWith(DefaultCritter other) {
-        if (this.distanceTo(other) <= this.collisionRadius + other.collisionRadius)
-            return true;
-        else
-            return false;
+        return this.distanceTo(other) <= this.collisionRadius + other.collisionRadius;
     }
 
 }
