c2fed7cdcd3f3b7ec10ba66cc239309202e55243
Nicol Visser <nicolvisser@yahoo.com>
2020-04-02 22:26:27 +0200

    Added LineSegment Class
    (Google difference between ray, line and line segment)
    
    Will be used to implement laser shooting

---
+++ src/LineSegment.java
@@ -0,0 +1,21 @@
+
+public class LineSegment {
+
+    Vector2D start;
+    Vector2D end;
+
+    public LineSegment(Vector2D start, Vector2D end) {
+        this.start = new Vector2D(start.x, start.y);
+        this.end = new Vector2D(end.x, end.y);
+    }
+
+    public LineSegment(Vector2D start, Vector2D direction, double length) {
+        this.start = new Vector2D(start.x, start.y);
+        this.end = start.add(direction.scale(length));
+    }
+
+    public void draw(double length) {
+        StdDraw.line(start.x, start.y, end.x, end.y);
+    }
+
+}
